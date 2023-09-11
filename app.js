576366a48188552c5253eb2e2cfeda560f240ccd
pyoja <113084653+pyoja@users.noreply.github.com>
2023-09-11 16:52:32 +0900

    app.js 기본설정

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//노드 연습
+//app.js
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/node-repeat");
+const db = mongoose.connection;
+
+db.on("error", (err) => console.log(err));
+db.once("open", () => console.log("db connected!"));
+
+app.use(express.json());
+app.use("/posts", postRouter);
+app.use("/comments", commentsRouter);
+
+app.listen(3000, () => {
+  console.log("server start!!!");
+});
