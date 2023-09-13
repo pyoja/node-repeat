f8a296f5cb37c97efaec764ce0e9210ce26094fd
pyoja <113084653+pyoja@users.noreply.github.com>
2023-09-13 17:49:22 +0900

    app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //app.js
-
 const express = require("express");
 const mongoose = require("mongoose");
 
+const postRouter = require("./routes/posts");
+const commentsRouter = require("./routes/comments");
+
 const app = express();
 
-mongoose.connect("mongodb://localhost/node-repeat");
+mongoose.connect("mongodb://localhost/node-basic");
 const db = mongoose.connection;
 
 db.on("error", (err) => console.log(err));
@@ -16,5 +18,5 @@ app.use("/posts", postRouter);
 app.use("/comments", commentsRouter);
 
 app.listen(3000, () => {
-  console.log("server start!!!");
+  console.log("server start");
 });
